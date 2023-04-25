b1b3cbda780def0326b38470b0403d2c4283358b
Adam <96267098+Adasiu1@users.noreply.github.com>
2023-04-26 00:55:50 +0200

    FirstScreen#1

--- App.js
+++ App.js
@@ -1,10 +1,77 @@
-import * as React from 'react';
+import React from 'react';
+import { StyleSheet, Text, View, Animated } from 'react-native';
+
+
+import FirstScreen from './navigation/BottomTab/screens/FirstScreen';
 import MainContainer from './navigation/MainContainer';
 
-function App(){
-  return(
-    <MainContainer/>
-  );
-}
+export default class App extends React.Component {
+  state = {
+    isLoading: true,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
 
-export default App;
+  componentDidMount() {
+    if (navigator && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          this.fetchWeather(position.coords.latitude, position.coords.longitude);
+        },
+        error => {
+          this.setState({
+            error: 'Error Getting Weather Conditions',
+            isLoading: false
+          });
+        }
+      );
+    } else {
+      this.setState({
+        error: 'Geolocation is not supported on this platform',
+        isLoading: false
+      });
+    }
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    this.setState({ isLoading: true });
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&exclude=${part}&appid=${bd770dc13eb778858626c0754394b8c1}&units=metric`
+      //https://api.openweathermap.org/data/3.0/weather?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}&units=metric
+    )
+    
+      .then(res => res.json())
+      .then(json => {
+        //console.log(json);
+        this.setState({
+          temperature: json.main.temp,
+          weatherCondition: json.weather[0].main,
+          isLoading: true
+        });
+      });
+  }
+
+  render() {
+    const { isLoading, weatherCondition, temperature } = this.state;
+    return (
+      <View style={styles.container}>
+        <MainContainer>
+          {isLoading ? (
+            <Text>Fetching The Weather</Text>
+          ) : (
+            <FirstScreen weather={weatherCondition} temperature={temperature} />
+          )}
+        </MainContainer>
+      </View>
+    );
+  }
+}  
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  }
+});
--- navigation/BottomTab/screens/FirstScreen.js
+++ navigation/BottomTab/screens/FirstScreen.js
@@ -1,12 +1,54 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { weatherConditions } from '../../../utils/WeatherCondtions';
 
-export default function FirstScreen({navigation}){
-  return(
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text
-          onPress={() => alert('This is the "First" screen. ')}
-          style={{ fontSize: 26, fontWeight: 'bold'}} >First Screen</Text>
+
+const FirstScreen = ({ weather , temperature }) => {
+  return (
+    <View style={styles.weatherContainer}>
+      <View style={styles.headerContainer}>
+        <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
+        <Text style={styles.tempText}>{temperature}˚</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text style={styles.title}>{weather}</Text>
+        <Text style={styles.subtitle}>Idziemy na Wyspę!</Text>
+      </View>
     </View>
   );
-}
+};
+
+
+const styles = StyleSheet.create({
+  weatherContainer: {
+    flex: 1,
+    backgroundColor: '#f7b733'
+  },
+  headerContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  tempText: {
+    fontSize: 48,
+    color: '#fff'
+  },
+  bodyContainer: {
+    flex: 2,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    paddingLeft: 25,
+    marginBottom: 40
+  },
+  title: {
+    fontSize: 48,
+    color: '#fff'
+  },
+  subtitle: {
+    fontSize: 24,
+    color: '#fff'
+  }
+});
+
+export default FirstScreen;
