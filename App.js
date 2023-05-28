8e7351931cc42220bd5eb6bb704144f8093ce5a5
gglaba <grzegorz.glaba@gmail.com>
2023-05-28 17:10:08 +0200

    1

--- App.js
+++ App.js
@@ -1,66 +1,66 @@
-// // import React from 'react';
-// // import { StyleSheet, Text, View, Animated } from 'react-native';
+// import React from 'react';
+// import { StyleSheet, Text, View, Animated } from 'react-native';
 
 
-// // import FirstScreen from './navigation/BottomTab/screens/FirstScreen';
-// // import MainContainer from './navigation/MainContainer';
-// // import SecondScreen from './navigation/BottomTab/screens/SecondScreen';
+// import FirstScreen from './navigation/BottomTab/screens/FirstScreen';
+// import MainContainer from './navigation/MainContainer';
+// import SecondScreen from './navigation/BottomTab/screens/SecondScreen';
 
-// // export default class App extends React.Component {
-// //   state = {
-// //     isLoading: false,
-// //     temperature: 0,
-// //     weatherCondition: null,
-// //     error: null
-// //   };
-
-
-
-// //   componentDidMount() {
-
-// //         this.fetchWeather();
-// //     }
+// export default class App extends React.Component {
+//   state = {
+//     isLoading: false,
+//     temperature: 0,
+//     weatherCondition: null,
+//     error: null
+//   };
 
 
-// //   fetchWeather(){
-// //     this.setState({ isLoading: true });
-// //     fetch(
-// //       `https://api.openweathermap.org/data/2.5/weather?q=Wroclaw&appid=d3ae3662e845fd57258468bc19e8316f&units=metric`
-// //       //https://api.openweathermap.org/data/3.0/weather?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}&units=metric
-// //     )
 
-// //       .then(res => res.json())
-// //       .then(json => {
-// //         console.log(json);
-// //         this.setState({
-// //           temperature: json.main.temp,
-// //           weatherCondition: json.weather[0].main,
-// //           isLoading: false
-// //         });
-// //       });
-// //   }
+//   componentDidMount() {
+
+//         this.fetchWeather();
+//     }
 
 
-// //   render() {
-// //     const { isLoading, weatherCondition, temperature } = this.state;
-// //     return (
-// //       <View style={styles.container}>
-// //         <MainContainer>
-// //             <FirstScreen  weather={weatherCondition} temperature={temperature} />
+//   fetchWeather(){
+//     this.setState({ isLoading: true });
+//     fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?q=Wroclaw&appid=d3ae3662e845fd57258468bc19e8316f&units=metric`
+//       //https://api.openweathermap.org/data/3.0/weather?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}&units=metric
+//     )
 
-// //         </MainContainer>
-// //       </View>
-// //     );
-// //   }
-// // }  
+//       .then(res => res.json())
+//       .then(json => {
+//         console.log(json);
+//         this.setState({
+//           temperature: json.main.temp,
+//           weatherCondition: json.weather[0].main,
+//           isLoading: false
+//         });
+//       });
+//   }
 
 
-// // const styles = StyleSheet.create({
-// //   container: {
-// //     flex: 1,
-// //     backgroundColor: '#fff'
-// //   }
-// // });
+//   render() {
+//     const { isLoading, weatherCondition, temperature } = this.state;
+//     return (
+//       <View style={styles.container}>
+//         <MainContainer>
+//             <FirstScreen  weather={weatherCondition} temperature={temperature} />
+
+//         </MainContainer>
+//       </View>
+//     );
+//   }
+// }  
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff'
+//   }
+// });
 
 // import React from 'react';
 // import { StyleSheet, Text, View, Animated } from 'react-native';
@@ -124,17 +124,77 @@
 //   }
 // });
 
+//import React from 'react';
+// import { StyleSheet, Text, View, Animated } from 'react-native';
+
+// import FirstScreen from './navigation/BottomTab/screens/FirstScreen';
+// import MainContainer from './navigation/MainContainer';
+// import SecondScreen from './navigation/BottomTab/screens/SecondScreen';
+
+// export default class App extends React.Component {
+//   state = {
+//     isLoading: false,
+//     temperature: 0,
+//     weatherCondition: null,
+//     error: null
+//   };
+
+//   componentDidMount() {
+//     this.fetchWeather();
+//   }
+
+//   fetchWeather() {
+//     this.setState({ isLoading: true });
+//     fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?q=Wroclaw&appid=d3ae3662e845fd57258468bc19e8316f&units=metric`
+//     )
+//       .then(res => res.json())
+//       .then(json => {
+//         console.log(json);
+//         this.setState({
+//           temperature: json.main.temp,
+//           weatherCondition: json.weather[0].main,
+//           isLoading: false
+//         });
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         this.setState({ error: 'Failed to fetch weather' });
+//       });
+//   }
+
+//   render() {
+//     const { isLoading, weatherCondition, temperature } = this.state;
+//     return (
+//       <View style={styles.container}>
+//         <MainContainer>
+//           <FirstScreen weatherCondition={weatherCondition} temperature={temperature} />
+//         </MainContainer>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff'
+//   }
+// });
+
+
 import React from 'react';
-import { StyleSheet, Text, View, Animated } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Asset } from 'expo-asset';
+
 
 import FirstScreen from './navigation/BottomTab/screens/FirstScreen';
 import MainContainer from './navigation/MainContainer';
-import SecondScreen from './navigation/BottomTab/screens/SecondScreen';
 
 export default class App extends React.Component {
   state = {
     isLoading: false,
-    temperature: 0,
+    temperature: null,
     weatherCondition: null,
     error: null
   };
@@ -146,29 +206,34 @@ export default class App extends React.Component {
   fetchWeather() {
     this.setState({ isLoading: true });
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=Wroclaw&appid=d3ae3662e845fd57258468bc19e8316f&units=metric`
+      'https://cors-anywhere.herokuapp.com/http://api.weatherapi.com/v1/current.json?key=dd60681d58474f4688c113344232605&q=Wroclaw&aqi=no'
     )
       .then(res => res.json())
       .then(json => {
-        console.log(json);
+        //console.log(json);
+        console.log(json.current.temp_c)
         this.setState({
-          temperature: json.main.temp,
-          weatherCondition: json.weather[0].main,
+          temperature: json.current.temp_c,
+          weatherCondition: json.current.condition.text,
           isLoading: false
         });
       })
       .catch(error => {
         console.error(error);
-        this.setState({ error: 'Failed to fetch weather' });
+        this.setState({ error: 'Error fetching weather' });
       });
   }
 
   render() {
-    const { isLoading, weatherCondition, temperature } = this.state;
+    const { isLoading, weatherCondition, temperature, error } = this.state;
     return (
       <View style={styles.container}>
         <MainContainer>
-          <FirstScreen weatherCondition={weatherCondition} temperature={temperature} />
+          {error ? (
+            <Text>{error}</Text>
+          ) : (
+            <FirstScreen weather={weatherCondition} temperature={temperature} />
+          )}
         </MainContainer>
       </View>
     );
--- navigation/BottomTab/screens/FirstScreen.js
+++ navigation/BottomTab/screens/FirstScreen.js
@@ -61,55 +61,110 @@
 
 // export default FirstScreen;
 
+// import React from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+// import { MaterialCommunityIcons } from '@expo/vector-icons';
+// import { weatherConditions } from '../../../utils/WeatherCondtions';
+
+// const FirstScreen = ({ weather, temperature }) => {
+//   return (
+//     <View style={styles.weatherContainer}>
+//       <View style={styles.headerContainer}>
+//         <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
+//         <Text style={styles.tempText}>{temperature}°C</Text> {/* Dodano styl dla temperatury */}
+//       </View>
+//       <View style={styles.bodyContainer}>
+//         <Text style={styles.title}>{weather}</Text>
+//         <Text style={styles.subtitle}>Idziemy na Wyspę!</Text>
+//       </View>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   weatherContainer: {
+//     flex: 1,
+//     backgroundColor: '#AED6F1'
+//   },
+//   headerContainer: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   },
+//   tempText: {
+//     fontSize: 48,
+//     color: '#fff'
+//   },
+//   bodyContainer: {
+//     flex: 2,
+//     alignItems: 'flex-start',
+//     justifyContent: 'flex-end',
+//     paddingLeft: 25,
+//     marginBottom: 40
+//   },
+//   title: {
+//     fontSize: 48,
+//     color: '#fff'
+//   },
+//   subtitle: {
+//     fontSize: 24,
+//     color: '#fff'
+//   }
+// });
+
+// export default FirstScreen;
+
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// const FirstScreen = ({ weather, temperature }) => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>Weather: {weather}</Text>
+//       <Text style={styles.text}>Temperature: {temperature}°C</Text>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   text: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+// });
+
+// export default FirstScreen;
+
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { weatherConditions } from '../../../utils/WeatherCondtions';
+import { StyleSheet, Text, View } from 'react-native';
 
 const FirstScreen = ({ weather, temperature }) => {
   return (
-    <View style={styles.weatherContainer}>
-      <View style={styles.headerContainer}>
-        <MaterialCommunityIcons size={48} name="weather-sunny" color={'#fff'} />
-        <Text style={styles.tempText}>{temperature}°C</Text> {/* Dodano styl dla temperatury */}
-      </View>
-      <View style={styles.bodyContainer}>
-        <Text style={styles.title}>{weather}</Text>
-        <Text style={styles.subtitle}>Idziemy na Wyspę!</Text>
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.weatherText}>Weather: {weather}</Text>
+      <Text style={styles.temperatureText}>Temperature: {temperature}°C</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  weatherContainer: {
+  container: {
     flex: 1,
-    backgroundColor: '#AED6F1'
-  },
-  headerContainer: {
-    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
   },
-  tempText: {
-    fontSize: 48,
-    color: '#fff'
+  weatherText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
-  bodyContainer: {
-    flex: 2,
-    alignItems: 'flex-start',
-    justifyContent: 'flex-end',
-    paddingLeft: 25,
-    marginBottom: 40
+  temperatureText: {
+    fontSize: 18,
   },
-  title: {
-    fontSize: 48,
-    color: '#fff'
-  },
-  subtitle: {
-    fontSize: 24,
-    color: '#fff'
-  }
 });
 
 export default FirstScreen;
