e6aafb6ecdf21b793ab04ed238e97a32b3ee446c
gglaba <grzegorz.glaba@gmail.com>
2023-05-31 00:32:01 +0200

    geolocation + events on front page

--- navigation/BottomTab/screens/FirstScreen.js
+++ navigation/BottomTab/screens/FirstScreen.js
@@ -2,15 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { weatherConditions } from '../../../utils/WeatherCondtions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
+import * as Location from 'expo-location';
 
 const FirstScreen = () => {
-  const [weather, setWeather] = useState(''); // Stan pogody
-  const [temperature, setTemperature] = useState(''); // Stan temperatury
-  const [weatherStyle, setWeatherStyle] = useState({}); // Stan stylu pogody
+  const [weather, setWeather] = useState('');
+  const [temperature, setTemperature] = useState('');
+  const [weatherStyle, setWeatherStyle] = useState({});
+  const [events, setEvents] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=Wroclaw%20PL';
+      const { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.error('Permission to access location was denied');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      const { latitude, longitude } = location.coords;
+
+      const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${latitude},${longitude}`;
       const options = {
         method: 'GET',
         headers: {
@@ -22,14 +36,13 @@ const FirstScreen = () => {
       try {
         const response = await fetch(url, options);
         const result = await response.json();
-        console.log(result)
         const { condition, temp_c } = result.current;
-        setWeather(condition.text); // Aktualizacja stanu pogody
-        setTemperature(temp_c); // Aktualizacja stanu temperatury
-
+        console.log(result);
+        setWeather(condition.text);
+        setTemperature(temp_c);
         const weatherKey = condition.text.trim();
         if (weatherKey in weatherConditions) {
-          setWeatherStyle(weatherConditions[weatherKey]); // Aktualizacja stanu stylu pogody
+          setWeatherStyle(weatherConditions[weatherKey]);
         }
       } catch (error) {
         console.error(error);
@@ -37,10 +50,57 @@ const FirstScreen = () => {
     };
 
     fetchData();
+
+
+    const getEventsData = async () => {
+      try {
+        const data = await AsyncStorage.getItem('eventsData');
+        if (data !== null) {
+          const parsedData = JSON.parse(data);
+          const filteredData = parsedData.filter(event => {
+            const eventDate = moment(event.date, 'YYYY-MM-DD');
+            const currentDate = moment().startOf('day');
+            const nextDate = moment().add(2, 'day').startOf('day');
+            return eventDate.isSameOrAfter(currentDate) && eventDate.isBefore(nextDate);
+            //return eventDate.isSameOrAfter(currentDate) && eventDate.isBefore(nextDate);
+          });
+          setEvents(filteredData);
+        } else {
+          setEvents([]);
+        }
+      } catch (error) {
+        console.error('Błąd podczas odczytywania danych z AsyncStorage:', error);
+      }
+    };
+
+    getEventsData();
+
+    // Wczytuj ponownie wydarzenia co pół minuty
+    const interval = setInterval(() => {
+      getEventsData();
+      //fetchData();
+    }, 1000);
+
+    return () => {
+      clearInterval(interval); // Zatrzymaj interwał po odmontowaniu komponentu
+    };
+
   }, []);
 
-  return ( // Wyświetlanie pogody
-    <View style={[styles.weatherContainer, { backgroundColor: weatherStyle.color }]}> 
+  
+  const formatEventDate = (eventDate) => {
+    const today = moment().startOf('day');
+    const tomorrow = moment().add(1, 'day').startOf('day');
+    
+    if (eventDate.isSame(today, 'day')) {
+      return 'Dzisiaj';
+    } else if (eventDate.isSame(tomorrow, 'day')) {
+      return 'Jutro';
+    }
+  };
+
+  return (
+    <View style={[styles.weatherContainer, { backgroundColor: weatherStyle.color }]}>
       <View style={styles.headerContainer}>
         <MaterialCommunityIcons size={48} name={weatherStyle.icon} color={'#fff'} />
         <Text style={styles.title}>{temperature}°C</Text>
@@ -49,6 +109,16 @@ const FirstScreen = () => {
         <Text style={styles.title}>{weatherStyle.title}</Text>
         <Text style={styles.subtitle}>{weatherStyle.subtitle}</Text>
       </View>
+      <View style={styles.eventsContainer}>
+          {events.map((event, index) => (
+            <View key={index}>
+              <Text style={styles.title}>{formatEventDate(moment(event.date, 'YYYY-MM-DD'))}: </Text>
+              <Text style={styles.eventTitle}>  {event.name}</Text>
+              <Text style={styles.eventDesc}>    {event.description}</Text>
+            </View>
+          ))}
+
+        </View>
     </View>
   );
 };
@@ -56,7 +126,7 @@ const FirstScreen = () => {
 const styles = StyleSheet.create({
   weatherContainer: {
     flex: 1,
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
   },
   headerContainer: {
@@ -66,15 +136,30 @@ const styles = StyleSheet.create({
   bodyContainer: {
     paddingLeft: 0,
     marginTop: 20,
+    alignItems: 'center'
+  },
+  eventsContainer: {
+    alignItems: 'flex-start',
+    marginHorizontal: '10%',
+    marginTop: '15%'
   },
   title: {
-    fontSize: 48,
+    fontSize: 44,
     color: '#fff'
   },
   subtitle: {
     fontSize: 24,
     color: '#fff'
+  },
+  eventTitle: {
+    fontSize: 24,
+    color: '#fff'
+  },
+  eventDesc: {
+    fontSize: 16,
+    color: '#fff'
   }
 });
 
 export default FirstScreen;
+
